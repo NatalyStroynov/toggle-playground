5448d29c12ff54c9c972125d7d97b95465905bf8
NatalyStroynov <maxim.natasha@gmial.com>
2025-07-31 14:00:58 +0300

    add component toggle demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { PlaygroundComponent } from './playground/playground.component';
+import { ToggleDemoComponent } from './toggle-demo/toggle-demo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,PlaygroundComponent],
+  imports: [RouterModule, RouterOutlet,ToggleDemoComponent,PlaygroundComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
+// app.routes.ts
 import { Routes } from '@angular/router';
+import { PlaygroundComponent } from './playground/playground.component';
+import { ToggleDemoComponent } from './toggle-demo/toggle-demo.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', component: PlaygroundComponent },
+  { path: 'toggle-demo', component: ToggleDemoComponent }
+];
